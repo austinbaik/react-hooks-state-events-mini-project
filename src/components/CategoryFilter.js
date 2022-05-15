b89a59277b3a79156954ce294902c7a3715a4144
Austin <baik.austin@gmail.com>
2022-05-15 07:29:19 -0700

    commit tree 5cc4a4437aa02c4d7f909c6a19a4d9105e323e1a

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,33 @@
 import React from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
+function CategoryFilter({ handleFilter, categories, selectedCategory }) {
+  // const [selected, setSelected] = useState(false)
+
+  // function filterCategory(category) {
+  //   handleFilter(category)
+  // }
+
+
+  const renderCategory = categories.map((category) => {
+    const className= category === selectedCategory ? "selected" : null
+    return (
+      <button
+        key={category}
+        className={className}
+        onClick={() => handleFilter(category)}
+        >  
+        {category}
+      </button>
+      //anonymous function does not run automatically 
+)
+})
+
+return (
+  <div className="categories" >
+    <h5>Category filters</h5>   
+    {renderCategory}
+  </div>
+);
 }
 
 export default CategoryFilter;
